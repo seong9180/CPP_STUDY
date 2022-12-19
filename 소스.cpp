804ef5c4289a8649f62339dab083b7ce3a8eb7fc
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2022-12-19 21:00:01 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -1,28 +1,30 @@
-//다시 시작하는 공부의 첫 걸음
-//2022.11.22 1일차
+//논리 연산자. 2022.12.19
+#include <iostream>
+using namespace std;
 
-//입출력 헤더 파일
-#include <iostream> 
-//문자열 헤더 파일
-#include <string> 
-//namespace 의 이름 = std , 왜 std 일까? std 의 의미는?
-using namespace std; 
- 
-int main(void)
+int main()
 {
-	/* 입출력 예제
-	cout << "Hello World!" << endl;
-	return 0;
+	int year;
+	bool result;
+
+	cout << "연도를 입력하세요: ";
+	cin >> year;
+	/*
+	윤년 계산법
+	1.4로 나누어 떨어지는 연도는 윤년으로 한다.
+	and
+	2.100으로 나누어 떨어지는 연도는 평년으로 한다.
+	or
+	3.400으로 나누어 떨어지는 연도는 윤년이다.
 	*/
 
-	string s1 = "Good";
-	string s2 = "Morning";
-	//" " : 띄어쓰기용 , \n 줄바꿈 
-	string s3 = s1 + " " + s2 + "!\n"; 
-	//endl을 쓰게 되면 \n이 없었어도 되는 게 아닐까
-	cout << s3;
-	string s4 = s1 + " " + s2 + "!";
-	//진짜네? s3 == s4 임을 확인할 수 있다.
-	cout << s4 << endl;
+	result = { (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) };
+	if (result == true) {
+		cout << year << "년도는 윤년입니다." << endl;
+	}
+	else {
+		cout << year << "년도는 평년입니다." << endl;
+	}
+
 	return 0;
 }
